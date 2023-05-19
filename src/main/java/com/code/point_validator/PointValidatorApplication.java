94d52aa33f8617da912950fb61023f61adf20973
NathanJFA <nathanjfa@outlook.com>
2023-05-18 21:05:29 -0300

    Commit inicial

---
+++ src/main/java/com/code/point_validator/PointValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.code.point_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/point_validator/PointValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.point_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
